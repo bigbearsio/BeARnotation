5846bc82721be9593a98013129f43af4392334ca
Varokas Panusuwan <varokas@panusuwan.com>
2017-06-11 12:10:17 +0700

    final tweak

--- ios/bearnotation/bearnotation/ViewController.swift
+++ ios/bearnotation/bearnotation/ViewController.swift
@@ -100,7 +100,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
                                          scale: SCNVector3(x: 0.07, y:0.07, z:0.07),
                                          rotation: SCNVector3(x:0, y: GLKMathDegreesToRadians(-90), z:0)))
         groupNode!.addChildNode(loadNode(file: "art.scnassets/billboard.dae",
-                                         loc: SCNVector3(x: -6, y:-2.5, z:0.5),
+                                         loc: SCNVector3(x: -8, y:-2.5, z:1),
                                          scale: SCNVector3(x: 0.005, y:0.005, z:0.005),
                                          rotation: SCNVector3(x: 0, y: GLKMathDegreesToRadians(-200), z: 0)))
         //GLKMathDegreesToRadians(-90)
